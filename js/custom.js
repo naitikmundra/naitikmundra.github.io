81d4df69a97693498dfad8d6f8934aa4a6de1400
naitikmundra <90164441+naitikmundra@users.noreply.github.com>
2024-04-27 13:43:34 +0530

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,13 @@
-
+// Function to set a cookie
+function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
 
 
     fetch('https://naitikmundra.github.io/navbar.html')
