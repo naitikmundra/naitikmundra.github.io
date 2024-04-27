2d44f3f9ba5702c833498b3defc94537ed9b13b2
naitikmundra <90164441+naitikmundra@users.noreply.github.com>
2024-04-27 13:44:04 +0530

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -8,6 +8,21 @@ function setCookie(name, value, days) {
     }
     document.cookie = name + "=" + (value || "") + expires + "; path=/";
 }
+// Function to get a cookie by name
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1, c.length);
+        }
+        if (c.indexOf(nameEQ) == 0) {
+            return c.substring(nameEQ.length, c.length);
+        }
+    }
+    return null;
+}
 
 
     fetch('https://naitikmundra.github.io/navbar.html')
